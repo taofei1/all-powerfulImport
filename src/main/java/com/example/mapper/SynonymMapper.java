6373e1d59520bd9934ad99e7c81ad9bce5205480
taofei1 <1396587180@qq.com>
2018-02-02 16:03:15 +0800

    111

---
+++ src/main/java/com/example/mapper/SynonymMapper.java
@@ -0,0 +1,34 @@
+package com.example.mapper;
+
+import com.example.domain.Synonym;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface SynonymMapper {
+
+    @Results(id = "synonymMapping", value = {
+            @Result(id = true, property = "id", column = "id"),
+            @Result(property = "table", column = "table_"),
+            @Result(property = "column", column = "column_"),
+            @Result(property = "columnName", column = "column_name"),
+            @Result(property = "synonymName", column = "synonym_name"),
+            @Result(property = "enable", column = "enable_")
+    })
+    @Select("select * from synonym")
+    List<Synonym> findAll();
+
+    @ResultMap("synonymMapping")
+    @Select("select * from synonym where id=#{id}")
+    Synonym findById(Integer id);
+
+    @Update("update synonym set table_=#{table},column_=#{column},column_name=#{columnName},synonym_name=#{synonymName},enable_=#{enable} where id=#{id}")
+    int updateSynonym(Synonym synonym);
+
+    @Insert("insert into synonym(table_,column_,column_name,synonym_name,enable_) values(#{table},#{column},#{columnName},#{synonymName},#{enable})")
+    int insertSynonym(Synonym synonym);
+
+    @Delete("delete from synonym where id=#{id}")
+    int deleteSynonym(Integer id);
+}
